86ae686dbcc8265ff851212910c25b9f0b14bcb8
DostonSuhrobov <doston.sanatovich@gmail.com>
2021-07-17 19:28:21 -0400

    first raw is ready and X can be clicked and set

--- src/App.js
+++ src/App.js
@@ -5,15 +5,40 @@ import Square from './component/Square';
 import { useState } from 'react';
 
 export default function App() {
-  const [board, setboard] = useState(['', '', '', '', '', '', '', '', '']);
+  const [board, setBoard] = useState(['', '', '', '', '', '', '', '', '']);
+  const [player, setPlayer] = useState('X');
+
+  const chooseSquare = square => {
+      setBoard(board.map((val,idx) => {
+        if( idx == square && val == "" ) {
+          return player;
+        }
+        return val;
+      }))
+  };
 
   return (
     <div className="App">
       <div className="board">
         <div className="row">
-          <Square val={board[0]} chooseSquare={() => alert(0)} />
-          <Square val={board[1]} chooseSquare={() => alert(1)} />
-          <Square val={board[2]} chooseSquare={() => alert(2)} />
+          <Square
+            val={board[0]}
+            chooseSquare={() => {
+              chooseSquare(0);
+            }}
+          />
+          <Square
+            val={board[1]}
+            chooseSquare={() => {
+              chooseSquare(1);
+            }}
+          />
+          <Square
+            val={board[2]}
+            chooseSquare={() => {
+              chooseSquare(2);
+            }}
+          />
         </div>
         <div className="row" />
         <div className="row" />
